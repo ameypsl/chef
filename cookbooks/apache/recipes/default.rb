5cc0751e8101196fd66fa8de2830a2007f470a9b
Amey Prabhudesai <amey_prabhudesai@persistent.com>
2017-12-13 10:54:02 +0000

    Updated Apache cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,12 +5,14 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
 
-package 'apache2'
-	package_name 'httpd' do
+package 'apache2' do
+	package_name 'httpd' 
 	action :install
 end
 
-service 'apache2'
-	service_name 'httpd' do
+service 'apache2' do
+	service_name 'httpd' 
 	action [:enable, :start]
 end
+
+include_recipe 'apache::websites'
